66f8bb450930b4886b5a3dea7ec0d3fee4705824
MarzollaB <marzollabenoit@gmail.com>
2025-05-25 22:58:55 +0200

    Update main.js

--- main.js
+++ main.js
@@ -25,9 +25,9 @@ function renderCarte() {
   const card = document.createElement('div');
   card.className = 'card';
 
-  // RECTO : une ou deux images
+  // FRONT : one or more images
   const front = document.createElement('div');
-  front.className = 'front ' + (data.images.length === 2 ? 'double' : 'single');
+  front.className = 'front ' + (data.images.length > 1 ? 'double' : 'single');
 
   // Ajout des images
   data.images.forEach(src => {
@@ -47,36 +47,27 @@ function renderCarte() {
   `;
   front.appendChild(navDiv);
 
-  // VERSO : tous les champs JSON
+  // VERSO : contenu structuré
   const back = document.createElement('div');
   back.className = 'back';
   back.innerHTML = `
     <h3>${data.titre}</h3>
-
     <h4>Symptômes visibles</h4>
     <ul>${data.verso.symptomes_visibles.map(s => `<li>${s}</li>`).join('')}</ul>
-
     <h4>Localisation dans le champ</h4>
     <p>${data.verso.localisation_dans_le_champ}</p>
-
     <h4>Feuilles atteintes</h4>
     <p>${data.verso.feuilles_atteintes}</p>
-
     <h4>Répartition sur la feuille</h4>
     <p>${data.verso.repartition_sur_la_feuille}</p>
-
     <h4>Symptômes secondaires</h4>
     <p>${data.verso.symptomes_secondaires}</p>
-
     <h4>Diagnostic</h4>
     <p>${data.verso.diagnostic}</p>
-
     <h4>Plan de lutte</h4>
     <ul>${data.verso.plan_de_lutte.map(p => `<li>${p}</li>`).join('')}</ul>
-
     <h4>Leviers agronomiques</h4>
     <ul>${data.verso.leviers_agronomiques.map(l => `<li>${l}</li>`).join('')}</ul>
-
     <h4>Notes complémentaires</h4>
     <ul>${data.verso.notes_complementaires.map(n => `<li>${n}</li>`).join('')}</ul>
   `;
